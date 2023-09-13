4df3581b5459ce12ce4784aee15a001be3e2414a
mafikidan <mierafg454@gmail.com>
2023-09-13 13:17:56 -0400

    base url

--- Evangadi-Forum-Feb-2023-G4/client/src/utility/axios.jsx
+++ Evangadi-Forum-Feb-2023-G4/client/src/utility/axios.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4500", // api url
+  // baseURL: "http://localhost:4500", // api url
   // baseURL: "https://gleaming-toad-shawl.cyclic.cloud/", // api url
-  // baseURL: "https://evangadi-forum-server-render.onrender.com",
+  baseURL: "https://evangadi-forum-server-render.onrender.com",
 
   withCredentials: true,
 });
